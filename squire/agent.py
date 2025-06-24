509994474510256adc91f006c72f7457a1bd8827
Nicholas Brown <nwbrown@gmail.com>
2025-06-24 18:08:41 -0400

    Get memories working

--- squire/agent.py
+++ squire/agent.py
@@ -11,17 +11,17 @@ def create_agent(cfg: dict) -> Agent:
                   model=Ollama(acfg['model']),
                   instructions=acfg['instructions'],
                   memory=create_memory(cfg),
-                  enable_agentic_memory=True
-                  )
+                  enable_agentic_memory=True)
     return agent
 
 def db_url(cfg: dict) -> str:
     db_cfg = cfg['db']
     auth = f"{db_cfg['creds']['user']}:{db_cfg['creds']['password']}@" if 'creds' in db_cfg else ""
-    return f"postgresql+psycopg://{auth}{db_cfg['host']}:{db_cfg['port']}/{db_cfg['schema']}"
+    return f"postgresql+psycopg://{auth}{db_cfg['host']}:{db_cfg['port']}/{db_cfg['db']}"
 
 def create_memory(cfg: dict) -> Memory:
     url = db_url(cfg)
-    return Memory(db=PostgresMemoryDb(table_name="memories", db_url=url))
+    db = PostgresMemoryDb(table_name="memories", db_url=url)
+    return Memory(db=db)
 
 
